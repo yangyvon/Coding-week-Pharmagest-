613fc0a4a6a39f55fa63fc5e37f3f53884941243
QuentinDS <quentin.da-silva@telecomnancy.net>
2018-01-15 17:27:32 +0100

    Filtres sur les lignes affichées dans le tableau 2D

--- UploadJSON/app.js
+++ UploadJSON/app.js
@@ -1,6 +1,6 @@
 
 var ImageUploadController = function ($scope, fileReader) {
-     console.log(fileReader)
+    //console.log(fileReader)
     $scope.getFile = function () {
         $scope.progress = 0;
         fileReader.readAsDataUrl($scope.file, $scope)
@@ -16,8 +16,10 @@ var ImageUploadController = function ($scope, fileReader) {
 };
 var JsonUploadController = function ($scope, $filter, ngTableParams, fileReader) {
 	
-	$scope.resXY = [];
-     console.log(fileReader)
+    //console.log(fileReader)
+	
+	$scope.display_limit1 = 0;
+	$scope.display_limit2 = 10;
  
     $scope.$on("fileProgress", function(e, progress) {
         $scope.progress = progress.loaded / progress.total;
@@ -36,23 +38,6 @@ var JsonUploadController = function ($scope, $filter, ngTableParams, fileReader)
             $scope.values.push(value);
           })
     });
-	
-	$scope.display_limit = 5;
-	
-	$scope.usersTable = new ngTableParams(
-		{
-		page: 1,
-        count: 10
-        }, 
-		{
-        total: $scope.resXY.length, 
-        getData: function ($defer, params) {
-		$scope.data = params.sorting() ? $filter('orderBy')($scope.resXY, params.orderBy()) : $scope.resXY;
-		$scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
-		$scope.data = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
-		$defer.resolve($scope.data);
-		}
-        });
 
     $scope.getValues = function(string) {
       
@@ -74,37 +59,23 @@ var JsonUploadController = function ($scope, $filter, ngTableParams, fileReader)
         }
 		
 		$scope.resXY = res;
-		console.log($scope.resXY);
+		//console.log($scope.resXY);
+		
+		$scope.usersTable = new ngTableParams({
+                page: 1,
+                count: 10
+            }, {
+                total: $scope.resXY.length, 
+                getData: function ($defer, params) {
+                    $scope.data = $scope.resXY.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                    $defer.resolve($scope.data);
+                }
+            });
       };
 
   };
 };
 
-// var JsonController=function($scope,$http){
-//   //Ici remplacer $scope.acteurs par le json input
-//   $scope.acteurs ={};
-
-//   $http.get('acteurs.json').success(function (data){
-//     $scope.acteurs = data;
-//     headers = /*data.*/data[0];
-//           $scope.keys = [];
-//           $scope.values = [];
-//           angular.forEach(headers, function(value, key) {
-//             $scope.keys.push(key);
-//             $scope.values.push(value);
-//           })
-//   });
-//   $scope.getTotalActeurs = function(){
-//         return $scope.acteurs.length;    
-//     }
-
-//   $scope.getJson = function(){
-//         return $scope.acteurs;
-//   }
-
-    
-
-//};
 app.directive("ngFileSelect",function(){
 
   return {
