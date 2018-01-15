1fd3c2e17d73cd2ef4bd8b690161e1501b285c1d
QuentinDS <quentin.da-silva@telecomnancy.net>
2018-01-15 15:54:07 +0100

    Tableau 2D

--- UploadJSON/app.js
+++ UploadJSON/app.js
@@ -15,6 +15,8 @@ var ImageUploadController = function ($scope, fileReader) {
  
 };
 var JsonUploadController = function ($scope, $filter, ngTableParams, fileReader) {
+	
+	$scope.resXY = [];
      console.log(fileReader)
  
     $scope.$on("fileProgress", function(e, progress) {
@@ -34,6 +36,23 @@ var JsonUploadController = function ($scope, $filter, ngTableParams, fileReader)
             $scope.values.push(value);
           })
     });
+	
+	$scope.display_limit = 5;
+	
+	$scope.usersTable = new ngTableParams(
+		{
+		page: 1,
+        count: 10
+        }, 
+		{
+        total: $scope.resXY.length, 
+        getData: function ($defer, params) {
+		$scope.data = params.sorting() ? $filter('orderBy')($scope.resXY, params.orderBy()) : $scope.resXY;
+		$scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
+		$scope.data = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
+		$defer.resolve($scope.data);
+		}
+        });
 
     $scope.getValues = function(string) {
       
@@ -45,29 +64,20 @@ var JsonUploadController = function ($scope, $filter, ngTableParams, fileReader)
         
         return res;
       };
+	  
+	$scope.getValues2 = function(string1, string2) {
+      
+        var res = [];
+        
+        for (var i = 0 ; i < $scope.lignes.length ; i++) {    
+          res.push({id:$scope.lignes[i][string1],val:$scope.lignes[i][string2]}); 
+        }
+		
+		$scope.resXY = res;
+		console.log($scope.resXY);
+      };
 
   };
-  
-	$scope.users = [{"id":1,"first_name":"Philip","last_name":"Kim","email":"pkim0@mediafire.com","country":"Indonesia","ip_address":"29.107.35.8"},
-                        {"id":2,"first_name":"Judith","last_name":"Austin","email":"jaustin1@mapquest.com","country":"China","ip_address":"173.65.94.30"},
-                        {"id":3,"first_name":"Julie","last_name":"Wells","email":"jwells2@illinois.edu","country":"Finland","ip_address":"9.100.80.145"},
-                        {"id":4,"first_name":"Gloria","last_name":"Greene","email":"ggreene3@blogs.com","country":"Indonesia","ip_address":"69.115.85.157"},
-                        {"id":50,"first_name":"Andrea","last_name":"Greene","email":"agreene4@fda.gov","country":"Russia","ip_address":"128.72.13.52"}];
- 
-	$scope.usersTable = new ngTableParams(
-		{
-		page: 1,
-        count: 10
-        }, 
-		{
-        total: $scope.users.length, 
-        getData: function ($defer, params) {
-		$scope.data = params.sorting() ? $filter('orderBy')($scope.users, params.orderBy()) : $scope.users;
-		$scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
-		$scope.data = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
-		$defer.resolve($scope.data);
-		}
-        });
 };
 
 // var JsonController=function($scope,$http){
@@ -118,5 +128,19 @@ app.directive("readText", function() {
         $scope.getTextFile();
       });
     }
+  }
+});
+
+app.directive('scrollPosition', function ($window) {
+  return function (scope, element, attrs) {      
+    var w = element[0];
+    //var w = $window;
+    angular.element(w).bind('scroll', function () {                  
+      scope.$apply(function () {                
+        if (w.scrollTop + w.offsetHeight >= w.scrollHeight) {          
+          scope.display_limit = scope.display_limit + 50;
+        }
+      });
+    });
   }
 });
