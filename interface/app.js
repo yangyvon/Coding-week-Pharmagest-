1ff05cdde2660cbc1b614577d055cedf079f9277
QuentinDS <quentin.da-silva@telecomnancy.net>
2018-01-16 16:55:07 +0100

    Some changes

--- interface/app.js
+++ interface/app.js
@@ -1,18 +1,3 @@
-var ImageUploadController = function ($scope, fileReader) {
-     console.log(fileReader)
-    $scope.getFile = function () {
-        $scope.progress = 0;
-        fileReader.readAsDataUrl($scope.file, $scope)
-                      .then(function(result) {
-                          $scope.imageSrc = result;
-                      });
-    };
- 
-    $scope.$on("fileProgress", function(e, progress) {
-        $scope.progress = progress.loaded / progress.total;
-    });
- 
-};
 var JsonUploadController = function ($scope, $filter, $timeout, ngTableParams, fileReader) {
 	
 	$scope.clock = "loading clock...";
@@ -25,43 +10,43 @@ var JsonUploadController = function ($scope, $filter, $timeout, ngTableParams, f
 
 	$timeout(tick, $scope.tickInterval);
 	
-	$scope.resXY = [];
-     console.log(fileReader)
+	$scope.getMonthsNames = function() {
+     var monthsNames= ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Août','Septembre','Octobre','Novembre','Décembre'];
+     return monthsNames;
+    }
+	
+	$scope.display_limit1 = 10;
  
     $scope.$on("fileProgress", function(e, progress) {
         $scope.progress = progress.loaded / progress.total;
     });
 
-    $scope.getTextFile = function () {
+   $scope.getTextFile = function () {
     $scope.progress = 0;
     fileReader.readAsText($scope.file, $scope).then(function(result) {
       $scope.jsonSrc = result;
           $scope.keys = [];
           $scope.values = [];
           $scope.lignes=JSON.parse($scope.jsonSrc);
-          header=$scope.lignes[0]
+          header=$scope.lignes[0];
+          $scope.type=null;
+          $scope.types=[];
           angular.forEach(header, function(value, key) {
             $scope.keys.push(key);
             $scope.values.push(value);
+            if (value.match(/(0\d{1}|1[0-2])\/([0-2]\d{1}|3[0-1])\/(19|20)\d{2}/)){ //expression régulière pour les dates
+              type='date';
+            }
+
+            else if (value.match(/^[\d ]+$/)){ //expression régulière correspondant à des digits en acceptant les espaces
+              type='number';
+            }
+            else {
+              type='string';  //par défaut c'est un string
+            }
+            $scope.types.push(type);
           })
     });
-	
-	$scope.display_limit = 5;
-	
-	$scope.usersTable = new ngTableParams(
-		{
-		page: 1,
-        count: 10
-        }, 
-		{
-        total: $scope.resXY.length, 
-        getData: function ($defer, params) {
-		$scope.data = params.sorting() ? $filter('orderBy')($scope.resXY, params.orderBy()) : $scope.resXY;
-		$scope.data = params.filter() ? $filter('filter')($scope.data, params.filter()) : $scope.data;
-		$scope.data = $scope.data.slice((params.page() - 1) * params.count(), params.page() * params.count());
-		$defer.resolve($scope.data);
-		}
-        });
 
     $scope.getValues = function(string) {
       
@@ -73,20 +58,112 @@ var JsonUploadController = function ($scope, $filter, $timeout, ngTableParams, f
         
         return res;
       };
-	  
-	$scope.getValues2 = function(string1, string2) {
-      
-        var res = [];
-        
-        for (var i = 0 ; i < $scope.lignes.length ; i++) {    
-          res.push({id:$scope.lignes[i][string1],val:$scope.lignes[i][string2]}); 
-        }
-		
-		$scope.resXY = res;
-		console.log($scope.resXY);
-      };
+    $scope.sumValuesByMonth = function(cleDate,cleY,annee) {
+      var months = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+      var date;
+      for (var i = 0; i < $scope.lignes.length; i++) {
+        date = new Date($scope.lignes[i][cleDate]);
+      if (date.getFullYear() == annee) {
+        var input = $scope.lignes[i][cleY]; //Pour gérer les espaces dans les chaines de caractères issues de l'offiReport
+        var processed = input.replace(/ /g, '');
+        var output = parseInt(processed, 10);
+        //Fait la somme par mois
+        months[date.getMonth()] += +output;
+      }
+    }
+    //console.log(months);
+    return(months);
+  };
 
   };
+
+  var uniqId = 1;
+  var selectedOptionDataList = new Array();
+
+function regenerateSelectbox(){
+    $('select.dyn-select').each(function(){
+        var _selector = $(this);
+        var _curVal = _selector.val();
+        
+        _selector.html(generateOption());
+        if(_curVal == ""){
+            _selector.append('<option value="'+ _curVal +'">select</option>');
+        }else{
+            _selector.append('<option value="'+ _curVal +'">'+ _curVal +'</option>');
+        }
+        _selector.val(_curVal);
+    })
+}
+
+function checkOptionExistences(listOptionVal){
+    var _found = false;
+    
+    $.each(selectedOptionDataList, function(inn, vnn){  
+        if(vnn.selectboxoption == listOptionVal){
+            _found = true;
+        }
+    });
+    
+    return _found;
+}
+
+function generateOption(){
+    var optionArr = new Array();
+    optionArr.push('<option value="">Choisir Axe</option>');
+    
+    $.each($scope.lignes, function(i, v){
+        if(!checkOptionExistences(v.value)){
+          optionArr.push('<option value="'+ v.value +'">'+ v.name +'</option>');
+        }
+    });
+    
+    return optionArr.join('\n');
+}
+
+function removeSelectedOptionFromList(param){
+    var tmpArrList = selectedOptionDataList;
+    selectedOptionDataList = new Array();
+    
+    $.each(tmpArrList, function(i, v){
+        if(param.selectboxid != v.selectboxid){
+            selectedOptionDataList.push({'selectboxid':v.selectboxid, 'selectboxoption':v.selectboxoption});
+        }
+    });
+}
+
+function selectedOptionList(param){
+    var _found = false;
+    $.each(selectedOptionDataList, function(i, v){
+        if(param.selectboxid == v.selectboxid){
+            _found = true;
+            v.selectboxoption = param.selectboxoption;
+        }
+    });
+    
+    if(!_found){
+        selectedOptionDataList.push({'selectboxid':param.selectboxid, 'selectboxoption':param.selectboxoption});
+    }
+}
+
+$('#ajoutAxe').click(function(){
+    if(selectedOptionDataList.length == $scope.lignes.length){
+        alert('Plus d axe disponible !');
+    }else{
+      $('div#axesY').append('<select class="dyn-select" id="select-'+ uniqId +'">'+ generateOption() +'</select>');
+      uniqId++;
+    }
+});
+
+$(document).on('change', 'select.dyn-select', function(){
+    var _selector = $(this);
+    if(_selector.val() == ""){
+        removeSelectedOptionFromList({'selectboxid':_selector.attr('id')});
+    }else{
+        selectedOptionList({'selectboxid':_selector.attr('id'), 'selectboxoption':_selector.val()});
+    }
+    
+    regenerateSelectbox();
+});
 };
 
 app.directive("ngFileSelect",function(){
